329141cd66ae9d5a9c5e1d5b4406ddfa1fb6acb0
IMEF <bolajifemi28@gmail.com>
2019-07-22 03:40:17 +0100

    Updates

---
+++ precache-manifest.fa8055d60c6144dff3188dd685639e65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4f9a4962c448cc665cf27d6204aef1",
+    "url": "/Twitter-Auth-React-Client/index.html"
+  },
+  {
+    "revision": "deb7ccdb70104f8300ea",
+    "url": "/Twitter-Auth-React-Client/static/css/main.f723ea97.chunk.css"
+  },
+  {
+    "revision": "1d4c72b0a83ae14f158f",
+    "url": "/Twitter-Auth-React-Client/static/js/2.7b4eeeea.chunk.js"
+  },
+  {
+    "revision": "deb7ccdb70104f8300ea",
+    "url": "/Twitter-Auth-React-Client/static/js/main.90f5dc47.chunk.js"
+  },
+  {
+    "revision": "a67dbbc12cc6c3c53f04",
+    "url": "/Twitter-Auth-React-Client/static/js/runtime~main.a058b3b1.js"
+  }
+]);
